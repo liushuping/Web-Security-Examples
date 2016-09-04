d2787dff5766fa737ca178f640ea5933d17d3716
Shuping LIU <lsp.lhh@gmail.com>
2016-09-04 12:26:09 +0800

    remove authroize from home controller

--- BooksOnline/Controllers/HomeController.cs
+++ BooksOnline/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BooksOnline.Controllers
 {
-    [Authorize]
+    // [Authorize]
     public class HomeController : Controller
     {
         public IActionResult Index()
